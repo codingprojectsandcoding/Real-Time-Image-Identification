610fe68abc602c17b98ac5dbf801f2dd143a1511
codingprojectsandcoding <95212367+codingprojectsandcoding@users.noreply.github.com>
2022-12-21 16:47:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,30 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
 }
-
-
-
+function modelLoaded() {
+console.log("Model Loaded!");
+}
+function draw() {
+image(video, 0, 0, 300, 300);
+classifier.classify(video, gotResult);
+}
+var previous_results = "";
+function gotResult(error, results) {
+if (error) {
+console.error(error);
+}
+else {
+if ((results[0].confidence> 0.5) && (previous_results != results[0].label)) {
+  console.log(results);
+previous_results = results[0].label;
+var synth = window.speechSynthesis;
+var speak_data = 'Object Detected Is -' + results[0].label;
+var utterThis = new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+document.getElementById("result_object_name").innerHTML = results[0].label;
+document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFized(3);
+}
+}
+}
